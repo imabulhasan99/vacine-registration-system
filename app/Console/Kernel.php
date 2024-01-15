c5309eb212d9cdee1a8b95ca8737c918c45a9732
imabulhasan99 <imabulhasan99@gmail.com>
2024-01-15 23:32:00 +0600

    code clean up

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->job(new ScheduleUnvaccinatedUsersJob)->everySecond();
+        $schedule->job(new ScheduleUnvaccinatedUsersJob)->weekdays();
         $schedule->job(new ProcessVaccinatedUsersJob)->weekdays();
         $schedule->job(new ResetVaccineCenterLimitJob)->daily();
     }
